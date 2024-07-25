3dbaf94a816896fb13c8399a5300e66c5dbeaf3f
Sam Kamarchuk <simeonkamarchuk@yandex.ru>
2024-07-25 14:35:17 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,8 +1,10 @@
 immutable_var = (1, 2, 3, 'string', 5.5, True)
-print('Immutable tuple: ',immutable_var)
-immutable_var[0] = 2
-print(immutable_var) #TypeError: 'tuple' object does not support item assignment - элемент кортежа не изменяется!
+  print('Immutable tuple: ',immutable_var)
+
+#TypeError: 'tuple' object does not support item assignment - элемент кортежа не изменяется!
+    # immutable_var[0] = 2
+    # print(immutable_var) 
 
 mutable_list = [1, 2, 3, 'string', 5.5, True]
 mutable_list[0] = 2
-print('Mutable list: ', mutable_list)
+  print('Mutable list: ', mutable_list)
